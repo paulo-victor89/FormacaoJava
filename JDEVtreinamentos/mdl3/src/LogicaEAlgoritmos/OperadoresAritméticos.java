b1c7e880677ab876aeaf9fd5374e1e89243b6641
Paulo Victor <pvictorcampelo@yahoo.com.br>
2020-10-02 19:38:33 -0300

    Adicionando arquivos ao git

---
+++ JDEVtreinamentos/mdl3/src/LogicaEAlgoritmos/OperadoresAritméticos.java
@@ -0,0 +1,22 @@
+package LogicaEAlgoritmos;
+
+public class OperadoresAritméticos {
+
+	public static void main(String[] args) {
+
+		double n1 = 50;
+
+		double n2 = 78;
+
+		System.out.println("Adição: " + (n1 + n2));
+
+		// Se o valor de n1 for menor que o de n2 vai ficar negativo
+		System.out.println("Subtração: " + (n1 - n2));
+		
+		System.out.println("Multiplicação: " + (n1 * n2));
+		
+		System.out.println("Divisão: " + (n1 / n2));
+
+	}
+
+}
